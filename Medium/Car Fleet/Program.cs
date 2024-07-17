4718e74ad69f1549bdf97a0370f8ef4e20edb3f4
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-18 01:38:43 +0200

    Solved - C# - Car Fleet

---
+++ Medium/Car Fleet/Program.cs
@@ -0,0 +1,5 @@
+﻿using Car_Fleet;
+
+var task = new Solution();
+int result = task.CarFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]);
+Console.WriteLine(result);
---
+++ Medium/Car Fleet/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Car_Fleet;
+
+
+public class Solution {
+	public int CarFleet(int target, int[] position, int[] speed) {
+		Array.Sort(position, speed);
+		Stack<float> fleets = [];
+
+		for (int car = 0; car < position.Length; car++) {
+			float time = (float)(target - position[car]) / speed[car];
+			while (fleets.Count > 0 && time >= fleets.Peek()) {
+				fleets.Pop();
+			}
+			fleets.Push(time);
+		}
+
+		return fleets.Count;
+	}
+}
