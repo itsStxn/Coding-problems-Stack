1b6ee995358146a6d263d1dcbaebd660253f09c8
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-17 22:57:09 +0200

    Solved - C# - Min Stack

---
+++ Medium/Min Stack/Program.cs
@@ -0,0 +1,11 @@
+﻿using Min_Stack;
+
+var minStack = new MinStack();
+
+minStack.Push(-2);
+minStack.Push(0);
+minStack.Push(-3);
+Console.WriteLine(minStack.GetMin());
+minStack.Pop();
+Console.WriteLine(minStack.Top());
+Console.WriteLine(minStack.GetMin());
---
+++ Medium/Min Stack/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Min_Stack;
+
+
+public class MinStack {
+		private int Min;
+		private int Value;
+		private MinStack? Below;
+
+	public MinStack() {
+		Min = int.MaxValue;
+		Value = int.MaxValue;
+		Below = null;
+	}
+	
+	public void Push(int val) {
+		Below = new MinStack {
+			Min = Min,
+			Value = Value,
+			Below = Below
+		};
+		Min = Math.Min(val, Min);
+		Value = val;
+	}
+	
+	public void Pop() {
+		Min = Below!.Min;
+		Value = Below.Value;
+		Below = Below.Below;
+	}
+	
+	public int Top() {
+		return Value;
+	}
+	
+	public int GetMin() {
+		return Min;
+	}
+}
