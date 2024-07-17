3a4985ceb6ff030de5ff42e64f037404b3d6ed0c
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-18 00:49:21 +0200

    Solved - C# - Evaluate Reverse Polish Notation

---
+++ Medium/Evaluate Reverse Polish Notation/Program.cs
@@ -0,0 +1,7 @@
+﻿using Evaluate_Reverse_Polish_Notation;
+
+var solution = new Solution();
+
+Console.WriteLine(solution.EvalRPN(["2", "1", "+", "3", "*"]));
+Console.WriteLine(solution.EvalRPN(["4", "13", "5", "/", "+"]));
+Console.WriteLine(solution.EvalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]));
---
+++ Medium/Evaluate Reverse Polish Notation/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Evaluate_Reverse_Polish_Notation;
+
+
+public class Solution {
+	private int Operate(string sign, int num1, int num2) {
+		return sign switch {
+			"+" => num1 + num2,
+			"-" => num1 - num2,
+			"*" => num1 * num2,
+			"/" => num1 / num2,
+			_ => default,
+		};
+	}
+	public int EvalRPN(string[] tokens) {
+		Stack<int> stack = [];
+		foreach (string token in tokens) {
+			if (!int.TryParse(token, out int num)) {
+				int num1 = stack.Pop();
+				int num2 = stack.Pop();
+				stack.Push(Operate(token, num2, num1));
+			} else stack.Push(num);
+		}
+		
+		return stack.Pop();
+	}
+}
